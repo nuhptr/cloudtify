8a244a591478f606ed7b826a9220d336c4d9d330
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2024-06-26 09:30:01 +0700

    refactor: :art: update globals css and added left sidebar layout

---
+++ components/layout/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const LeftSidebar = () => {
+   return (
+      <section className="left_sidebar">
+         <nav className="flex flex-col gap-6"></nav>
+      </section>
+   )
+}
